ced7646244ba3afddd8f560374c44be35f293c93
Julien Moratalla <julien.moratalla@gmail.com>
2021-07-01 23:03:05 +0200

    Init react tpl + parcel + typescript

---
+++ src/app.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+const App = () => {
+  return <Typography>React App Builded with parcel 2 !</Typography>;
+};
+
+export default App;
